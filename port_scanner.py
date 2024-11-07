6919cf0e97ba0dab6ee178a03e5ccfd341d6d759
user <user@gmail.com>
2024-11-07 22:25:39 +1100

    added validation for arguments

--- port_scanner.py
+++ port_scanner.py
@@ -2,13 +2,29 @@ import argparse
 from socket import socket, AF_INET, SOCK_STREAM
 from typing import Tuple, List
 
+banner = \
+"""
+  _____           _      _____                                 
+ |  __ \         | |    / ____|                                
+ | |__) |__  _ __| |_  | (___   ___ __ _ _ __  _ __   ___ _ __ 
+ |  ___/ _ \| '__| __|  \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
+ | |  | (_) | |  | |_   ____) | (_| (_| | | | | | | |  __/ |   
+ |_|   \___/|_|   \__| |_____/ \___\__,_|_| |_|_| |_|\___|_|   
+"""
+
 
 def main():
+    print(banner)
+    print('=' * 100)
+
     host, ports = cli()
 
-    print(f'[*] Starting Scan on host: {host}...')
+    print(f'[*] Starting Port Scan on host: {host}...')
+
     for port in ports:
         scan_port(host, port)
+    
+    print(f'[*] Port Scan Ended for host: {host}')
 
 
 def scan_port(host: str, port: int) -> None:
@@ -19,10 +35,10 @@ def scan_port(host: str, port: int) -> None:
         try:
             sock.connect((host, port))
         except ConnectionRefusedError:
-            print(f'[!] Connection Refused on port: {port}')
+            print(f'[!] Connection Refused for port: {port}')
             return
         except TimeoutError:
-            print(f'[!] Connection Timed out on port: {port}')
+            print(f'[!] Connection Timed out for port: {port}')
             return
         
         print(f'[*] Connected to port: {port}')
@@ -35,8 +51,29 @@ def cli() -> Tuple[str, List[int]]:
     parser.add_argument('ports')
 
     args = parser.parse_args()
-    host = args.host
-    ports = args.ports
+    host: str =  args.host
+    octets = host.split('.', 3)
+    for octet in octets:
+        if not octet.isnumeric() or not (0 <= int(octet) <= 255):
+            raise ValueError(f'{host} is not an IPv4 address')
 
+    ports: str = args.ports
+    if ',' not in ports and '-' not in ports:
+        try:
+            ports = [int(ports)]
+        except TypeError:
+            raise ValueError(f'{ports} is not a port')
+    elif ',' in ports:
+        try:
+            ports = [int(port) for port in ports.split(',')]
+        except:
+            raise ValueError(f'{ports} is not a port')
+    elif '-' in ports:
+        try:
+            ports = list(range(int(ports.split('-', 1)[0]), int(ports.split('-', 1)[1]) + 1))
+        except:
+            raise ValueError(f'{ports} is not a port')
+
+    return host, ports
 
 main()

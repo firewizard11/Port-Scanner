92383db757c633f52c652798c2389bd1d70a3758
Sam <seriousemail178@gmail.com>
2025-11-29 10:04:58 +1100

    Added concurrent_scan **still testing**

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from modules import port_scanner
 
 target_host = "10.48.161.248"
-target_ports = [21, 22, 80, 443]
+target_ports = list(range(1,65535+1))
 timeout = 1
+max_probes = 200
 scanner = port_scanner.PortScanner(target_host, target_ports, timeout)
 
 print("=== Port Scanner ===")
@@ -16,7 +17,7 @@ print()
 print("Timeout: " + str(timeout))
 
 print("Starting TCP Scan")
-open_ports = scanner.sequential_scan()
+open_ports = scanner.concurrent_scan()
 
 print("Results:")
 for port in target_ports:
--- modules/port_scanner.py
+++ modules/port_scanner.py
@@ -1,4 +1,5 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 MAX_PORT = 65535
@@ -6,7 +7,7 @@ MAX_PORT = 65535
 
 class PortScanner():
 
-    def __init__(self, target_host: str, target_ports: list[int], timeout: int = 1):
+    def __init__(self, target_host: str, target_ports: list[int], timeout: int = 1, max_probes: int = 1):
         self.target_host = target_host
 
         self.target_ports = target_ports
@@ -15,6 +16,11 @@ class PortScanner():
         self.timeout = timeout # The amount of time to wait before ending a probe
         if self.timeout < 1:
             raise ValueError("timeout should be greater than 0")
+        
+        self.max_probes = max_probes
+        if self.max_probes < 1:
+            raise ValueError("max_probes should be 1 or higher")
+
 
     def sequential_scan(self) -> list[int]:
         """Scan each port in target_ports one after the other
@@ -28,6 +34,27 @@ class PortScanner():
                 open_ports.append(port)
 
         return open_ports
+    
+    def concurrent_scan(self) -> list[int]:
+        """Scan self.max_probes number of ports in target_ports till done
+        :returns:
+        open_ports(list[int]): List of port numbers that returned True with the probe
+        """
+        open_ports = []
+
+        with ThreadPoolExecutor(self.max_probes) as pool:
+            futures = {pool.submit(self.tcp_probe, port):port for port in self.target_ports}
+
+            for future in as_completed(futures):
+                port = futures[future]
+                print(port)
+                is_open = future.result()
+
+                if is_open:
+                    open_ports.append(port)
+
+        return open_ports
+
 
     def tcp_probe(self, target_port: int) -> bool:
         """ Completes full 3 way handshake to test port

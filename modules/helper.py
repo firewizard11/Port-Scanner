3a8e3ca6c40d39c50e06c3a1959e7e6918c0822b
sam <seriousemail178@gmail.com>
2025-12-02 18:35:39 +1100

    Moved validation functions to this file

---
+++ modules/helper.py
@@ -0,0 +1,19 @@
+def validate_port_list(port_list: list[int]):
+    if not isinstance(port_list, list):
+        raise TypeError("portlist should be a list")
+    
+    if len(port_list) < 1:
+        raise ValueError(f"portlist should have atleast 1 port (current={len(port_list)})")
+    
+    if len(port_list) > 65535:
+        raise ValueError(f"portlist should have less than 65535 ports (current={len(port_list)})")
+    
+    for port in port_list:
+        validate_port(port)
+
+def validate_port(port: int):
+    if not isinstance(port, int):
+        raise TypeError("port should be of type int")
+    
+    if not (0 < port <= 65535):
+        raise ValueError("port should be between 1 and 65535 (inclusive)")
